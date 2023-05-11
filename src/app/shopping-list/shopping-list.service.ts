ade7696a28a274b62fbaead3d8d316cb286918ec
Winton Wang <winton168@hotmail.com>
2023-05-10 23:49:37 -0400

    Save 223 chapter

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -8,6 +8,7 @@ import { Ingredient } from '../shared/ingredient.model';
 export class ShoppingListService {
 
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
 
     private ingredients: Ingredient[] = [
@@ -17,6 +18,10 @@ export class ShoppingListService {
 
     getIngredients() {
         return this.ingredients.slice();
+    }
+
+    getIngredient(index: number){
+        return this.ingredients[index];
     }
 
     addIngredient(ingredient: Ingredient){
